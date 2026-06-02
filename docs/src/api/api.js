f1a34bf3fa7c5e97398a7ba5784e26cc13683399
JavieraJPG <javierapaz592@gmail.com>
2026-06-02 10:41:25 -0400

    Actualizar URL de CORS y corregir la base de API en el entorno de producción

--- docs/src/api/api.js
+++ docs/src/api/api.js
@@ -4,7 +4,7 @@
 //const API_BASE_URL = (typeof import.meta !== 'undefined' && import.meta.env && import.meta.env.VITE_API_URL) 
  //   ? import.meta.env.VITE_API_URL 
  //   : "http://localhost:8080/api";
-const API_BASE_URL = "https://homiewood.onrender.com/api";
+const API_BASE_URL = "https://homiewood-p3p5.onrender.com/api";
 
 //const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:8080/api";
 
